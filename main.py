4a8e68674232d3e662e447d758b02f81e196bbc4
Ayden Fleming <48303729+LiteEagle262@users.noreply.github.com>
2023-04-15 01:12:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,59 +2,55 @@ import requests
 from time import sleep
 from discord_webhook import DiscordWebhook, DiscordEmbed
 
-cookie = 'cookiehere' # for the cookie, i recomend having a bot account as the cookie as its easier, to refresh the cookie to be used by any ip use this: https://rblxcopy.net/cookierefresh.php
+cookie = 'cookiehere' # for the cookie, I recommend having a bot account as the cookie as it's easier, to refresh the cookie to be used by any IP use this: https://rblxcopy.net/cookierefresh.php
 webhook_url = 'webhookhere'
 group_id = '696969' #your group id
 headers = {'Cookie':f'.ROBLOSECURITY={cookie}'}
-webhook = DiscordWebhook(url=webhook_url, content='<@785585779429867561>') # example of how to ping someone, its <@> and in the middle is your user id on discord
+webhook = DiscordWebhook(url=webhook_url, content='<@785585779429867561>') # example of how to ping someone, it's <@> and in the middle is your user ID on discord
 
-def getdata():
-    r = requests.get(f"https://economy.roblox.com/v2/groups/{group_id}/transactions?cursor=&limit=100&sortOrder=Asc&transactionType=Sale",headers=headers)
-    jso = r.json()
-    return jso["data"][0]
+def get_data():
+    try:
+        r = requests.get(f"https://economy.roblox.com/v2/groups/{group_id}/transactions?cursor=&limit=100&sortOrder=Asc&transactionType=Sale",headers=headers)
+        r.raise_for_status()
+        jso = r.json()
+        return jso.get("data", [])[0]
+    except requests.exceptions.HTTPError as err:
+        print(f"HTTP error occurred: {err}")
+    except Exception as err:
+        print(f"An error occurred: {err}")
+
+def send_webhook(data):
+    user_id = data['agent'].get('id')
+    username = data['agent'].get('name')
+    product_name = data['details'].get('name')
+    product_price = data['currency'].get('amount')
+    desc = f"""
+    ```fix
+    User ID: {user_id}
+    Username: {username}
+    Product Name: {product_name}
+    Product Price: {product_price} Robux
+    ```
+    """
+    embed = DiscordEmbed(title='New Sale!!', description=desc, color='FFFF00')
+    embed.set_thumbnail(url=f"https://www.roblox.com/headshot-thumbnail/image?userId={user_id}&width=420&height=420&format=png")
+    embed.set_timestamp()
+    embed.set_footer(text='Made by https://liteeagle.me/')
+    webhook.add_embed(embed)
+    response = webhook.execute()
 
 def start():
     print("Running")
-    compare()
-    return
-
-def getnew():
-    r = requests.get(f"https://economy.roblox.com/v2/groups/{group_id}/transactions?cursor=&limit=100&sortOrder=Asc&transactionType=Sale",headers=headers)
-    jso = r.json()
-    return jso["data"][0]
-
-def compare():
-    try:
-        predat = getdata()
-        print(predat)
-        while getnew() == predat:
+    while True:
+        try:
+            prev_data = get_data()
+            print(prev_data)
             sleep(1)
-        else:
-            r = requests.get(f"https://economy.roblox.com/v2/groups/{group_id}/transactions?cursor=&limit=100&sortOrder=Asc&transactionType=Sale",headers=headers)
-            jso = r.json()
-            user_id = jso['data'][0]['agent']['id']
-            username = jso['data'][0]['agent']['name']
-            product_name = jso['data'][0]['details']['name']
-            Product_Price = jso['data'][0]['currency']['amount']
-            desc = f"""
-    ```fix
-    User Id: {user_id}
-    Username: {username}
-    Product Name: {product_name}
-    Product Price: {Product_Price} Robux
-    ```
-    """
-            embed = DiscordEmbed(title='New Sale!!', description=desc, color='FFFF00')
-            embed.set_thumbnail(url=f"https://www.roblox.com/headshot-thumbnail/image?userId={user_id}&width=420&height=420&format=png")
-            embed.set_timestamp()
-            embed.set_footer(text='Made by LiteEagle262#2777')
-            webhook.add_embed(embed)
-            response = webhook.execute()
-            start()
-            return
-    except:
-        print("A error has occurred, restarting")
-        start()
-        return
+            new_data = get_data()
+            if new_data != prev_data:
+                send_webhook(new_data)
+        except:
+            print("An error has occurred, restarting")
+            continue
 
 start()
